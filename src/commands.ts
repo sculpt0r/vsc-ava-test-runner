9c1e61da3023b21a2f539869471a6428a44a7bff
sculpt0r <github@sculpt0r.net>
2022-06-04 16:14:49 +0200

    Add custom commands

---
+++ src/commands.ts
@@ -0,0 +1,10 @@
+export function runTestsInFile(){
+	//get active file
+	//run
+	console.log('all test runned!');
+}
+
+export function runTestsInFile_debug() {
+	console.log('all test runned __ debug!');
+
+}
