cba405a1acf40350a0f11338ed5281611ec39ede
sculpt0r <github@sculpt0r.net>
2022-06-22 04:29:18 +0200

    Fix undefined in command arg

--- src/commands.ts
+++ src/commands.ts
@@ -1,10 +1,11 @@
 import * as vscode from 'vscode';
 
-export function runTestsInFile( args: string ){
+export function runTestsInFile( args?: string ){
 	console.log( 'all test runned!' );
 	const activeFilePath = vscode.window.activeTextEditor?.document.uri.path;
 	if( activeFilePath !== undefined )
 	{
+		args ??= '';
 		const cmd = `npx ava --verbose ${activeFilePath}${args}`;
 		runTerminalCmd( cmd );
 	} else {
